7006ba59a8e74328973319ba36f8836c07345553
vahagn85 <v.ulikhanyan@yandex.ru>
2022-03-03 19:35:16 +0400

    new added

--- src/App.js
+++ src/App.js
@@ -3,64 +3,68 @@ import { ListItem } from './components/ListItem';
 import { TaskField } from './components/TaskField';
 
 function App() {
-  const [tasks, setTasks] = React.useState([
-    {
-      text: 'Изучить ReactJS',
-      completed: true,
-    },
-    {
-      text: 'Разработать ToDo на ReactJS',
-      completed: false,
-    },
-  ]);
+	const [tasks, setTasks] = React.useState([
+		{
+			text: 'Изучить ReactJS',
+			completed: true,
+		},
+		{
+			text: 'Разработать ToDo на ReactJS',
+			completed: false,
+		},
+		{
+			text: 'Разработать ToDo на ReactJS ---2',
+			completed: false,
+		},
+	]);
 
-  const onToggleCompleted = (index) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task, curIdx) =>
-        index === curIdx
-          ? {
-              ...task,
-              completed: !task.completed,
-            }
-          : task,
-      ),
-    );
-  };
+	const onToggleCompleted = (index) => {
+		setTasks((prevTasks) =>
+			prevTasks.map((task, curIdx) =>
+				index === curIdx
+					? {
+						...task,
+						completed: !task.completed,
+					}
+					: task,
+			),
+		);
+	};
 
-  const onRemoveTask = (index) => {
-    setTasks((prevTasks) => prevTasks.filter((_, curIdx) => index !== curIdx));
-  };
+	const onRemoveTask = (index) => {
+		setTasks((prevTasks) => prevTasks.filter((_, curIdx) => index !== curIdx));
+	};
 
-  const onAddTask = (text) => {
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      {
-        text,
-        completed: false,
-      },
-    ]);
-  };
+	const onAddTask = (text) => {
+		setTasks((prevTasks) => [
+			...prevTasks,
+			{
+				text,
+				completed: false,
+			},
+		]);
+	};
 
-  return (
-    <div className="todo">
-      <div className="todo__header">
-        <h4>Список задач</h4>
-      </div>
-      <TaskField onAddTask={onAddTask} />
-      <div className="todo__list">
-        {tasks.map((task, index) => (
-          <ListItem
-            key={index}
-            index={index}
-            text={task.text}
-            completed={task.completed}
-            onToggleCompleted={onToggleCompleted}
-            onRemoveTask={onRemoveTask}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="todo">
+			<div className="todo__header">
+				<h4>Список задач</h4>
+			</div>
+			<TaskField onAddTask={onAddTask} />
+			<div className="todo__list">
+				{tasks.map((task, index) => (
+					<ListItem
+						key={index}
+						index={index}
+						text={task.text}
+						completed={task.completed}
+						onToggleCompleted={onToggleCompleted}
+						onRemoveTask={onRemoveTask}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
