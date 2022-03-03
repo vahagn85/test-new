078fa89d73b175494a87508e59e4862dd9a07023
vahagn85 <v.ulikhanyan@yandex.ru>
2022-03-03 20:04:51 +0400

    Updates

---
+++ precache-manifest.9fd6431c7b84c0f9de99bff5ed6642d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bc100606519201c87b262d737f84d8",
+    "url": "/test-new/index.html"
+  },
+  {
+    "revision": "b8bd4137665421e0bea4",
+    "url": "/test-new/static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/test-new/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-new/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bd4137665421e0bea4",
+    "url": "/test-new/static/js/main.7bb875e0.chunk.js"
+  },
+  {
+    "revision": "87938f8893a5ecc54b65",
+    "url": "/test-new/static/js/runtime-main.600cf47d.js"
+  }
+]);
